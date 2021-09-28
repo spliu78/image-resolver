b73ecc955c97e9a1aaa0a9346a6ff6eadcabd6b8
SP Liu <spliu787273@gmail.com>
2021-09-28 15:55:56 +0800

    feat: add files

--- project-init.sh
+++ project-init.sh
@@ -3,4 +3,4 @@ npm install --save-dev husky @commitlint/config-conventional @commitlint/cli
 npx husky install
 npx husky add .husky/commit-msg 'npx --no-install commitlint --edit "$1"'
 npm set-script prepare "husky install"
-echo "exports = {extends: ['@commitlint/config-conventional']};" >commitlint.config.js
+echo "module.exports = { extends: ['@commitlint/config-conventional'] };" >commitlint.config.js
